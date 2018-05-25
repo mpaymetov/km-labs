baedb83535f312d57635ba2e0253988f7168e639
mpaymetov <muxa147@gmail.com>
2018-05-25 19:06:32 +0300

    добавил поиск мостов

--- lab2/lab2_1/test.bat
+++ lab2/lab2_1/test.bat
@@ -3,6 +3,9 @@ set PROGRAM="%~1"
 %PROGRAM% input.txt
 if ERRORLEVEL 1 GOTO err
 
+rem %PROGRAM% input2.txt
+rem if ERRORLEVEL 1 GOTO err
+
 echo OK
 exit 0
 
--- lab2/lab2_2/test.bat
+++ lab2/lab2_2/test.bat
@@ -3,6 +3,12 @@ set PROGRAM="%~1"
 %PROGRAM% input.txt
 if ERRORLEVEL 1 GOTO err
 
+%PROGRAM% input2.txt
+if ERRORLEVEL 1 GOTO err
+
+%PROGRAM% input3.txt
+if ERRORLEVEL 1 GOTO err
+
 echo OK
 exit 0
 
